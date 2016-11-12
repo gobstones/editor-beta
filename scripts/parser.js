c3629b7f8c06ebddd35fea9f4992ef9ee2f62b98
AlvarezAriel <alvarez.ariel.work@gmail.com>
2016-11-12 09:20:57 -0300

    Update 2016-11-12T12:20:49.277Z

--- scripts/parser.js
+++ scripts/parser.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
@@ -8,26 +8,27 @@ var Parser = function () {
   function Parser() {
     _classCallCheck(this, Parser);
 
-    console.log(window.gsWeblangCore);
-
     this.Context = window.gsWeblangCore.Context;
-    this.buildAst = window.gsWeblangCore.grammar.parse;
+    this.getParser = window.gsWeblangCore.getParser;
+    console.log(window.gsWeblangCore);
   }
 
   _createClass(Parser, [{
-    key: "parse",
+    key: 'parse',
     value: function parse(sourceCode) {
-      return this.buildAst(sourceCode);
+      console.log('PARSING');
+      console.log(sourceCode);
+      return this.getParser().parse(sourceCode);
     }
   }, {
-    key: "interpret",
+    key: 'interpret',
     value: function interpret(ast, initialState) {
       var context = this._createContext(initialState);
       ast.interpret(context);
       return context;
     }
   }, {
-    key: "_createContext",
+    key: '_createContext',
     value: function _createContext(initialState) {
       var context = new this.Context();
       context.board().sizeX = initialState.size.x;
